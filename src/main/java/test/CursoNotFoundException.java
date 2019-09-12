9c6d55fa0fbce036e3f2c23d83e95d307f8c3ae9
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 03:20:17 -0400

    Added CursoNotFoundException.java

--- src/main/java/test/CursoNotFoundException.java
+++ src/main/java/test/CursoNotFoundException.java
@@ -1,5 +1,7 @@
 package main.java.test;
 
-public class CursoNotFoundException {
-
+public class CursoNotFoundException extends  RuntimeException{
+    CursoNotFoundException(Long id){
+        super("No se pudo encontrar el curso "+id);
+    }
 }
