632308f0d92c160f22ae3f593f3c03ecc6c45860
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 01:26:27 -0400

    Added Curso.java

--- src/main/java/test/Curso.java
+++ src/main/java/test/Curso.java
@@ -1,1 +1,24 @@
+package main.java.test;
 
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Curso {
+
+    private @Id @GeneratedValue Long id;
+    private String grado;
+    private String paralelo;
+
+    Curso() { }
+
+    public Curso(String grado, String paralelo) {
+        this.grado = grado;
+        this.paralelo = paralelo;
+    }
+}
