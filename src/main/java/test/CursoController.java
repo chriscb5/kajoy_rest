7bf69c40622200b91c90be133a88631ff29be015
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-09-12 03:08:53 -0400

    Added CursoController.java

---
+++ src/main/java/test/CursoController.java
@@ -0,0 +1,30 @@
+package main.java.test;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CursoController {
+    private final CursoRepository repository;
+
+    public CursoController(CursoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/cursos")
+    List<Curso> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/cursos")
+    Curso newCurso(@RequestBody Curso newCurso) {
+        return repository.save(newCurso);
+    }
+}
